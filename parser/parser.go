ff67e082f47bdc8d6e89cbd0d63d1851a6a763f8
Adrian Thomas <adriancleve1@gmail.com>
2024-07-09 20:51:55 -0400

    If Else expressions

--- parser/parser.go
+++ parser/parser.go
@@ -98,7 +98,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 	for !p.curTokenIs(token.EOF) {
-		statement := p.parseStatements()
+		statement := p.parseStatement()
 		if statement != nil {
 			program.Statements = append(program.Statements, statement)
 		}
@@ -110,7 +110,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 // Parses next possible statement.
 // If the current token doesn't start a new supported statement
 // we return nil
-func (p *Parser) parseStatements() ast.Statement {
+func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
@@ -250,6 +250,46 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 	}
 
 	return expression
+}
+
+func (p *Parser) parseIfExpression() ast.Expression {
+	expression := &ast.IfExpression{Token: p.curToken}
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+	p.nextToken()
+	expression.Condition = p.parseExpression(LOWEST)
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+		expression.Alternative = p.parseBlockStatement()
+	}
+
+	return expression
+}
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+	p.nextToken()
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return block
 }
 
 // Check if current token is of a certain token.TokenType (t)
