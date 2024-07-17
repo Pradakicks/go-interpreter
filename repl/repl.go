44af7b199b5ec26e068878cbddf055aeaef41bb0
Adrian Thomas <adriancleve1@gmail.com>
2024-07-17 19:11:34 -0400

    Bindings (let statements), Added Environment parameter to eval

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-interpreter/evaluator"
 	"github.com/go-interpreter/lexer"
+	"github.com/go-interpreter/object"
 	"github.com/go-interpreter/parser"
 )
 
@@ -28,6 +29,7 @@ const MONKEY_FACE = `
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Print(PROMPT)
 		scanned := scanner.Scan()
@@ -42,7 +44,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
